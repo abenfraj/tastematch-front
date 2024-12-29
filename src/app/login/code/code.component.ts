36483c7bcb20cd0226d2ba64b3d6898444d95065
abenfraj <ayoub.benfraj013@gmail.com>
2024-12-29 16:23:33 +0100

    refix code component

--- src/app/login/code/code.component.ts
+++ src/app/login/code/code.component.ts
@@ -1,53 +1,46 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Location } from '@angular/common';
-import { ProgressBarComponent } from '../../shared/components/progress-bar/progress-bar.component';
 import { slideAnimation } from '../../shared/animations/route-animations';
 
 @Component({
   selector: 'app-code',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './code.component.html',
   styleUrls: ['./code.component.scss'],
   animations: [slideAnimation],
-  standalone: true,
-  imports: [CommonModule, ProgressBarComponent]
+  host: { '[@routeAnimations]': '' }
 })
 export class CodeComponent {
+  formattedCode = '';
   code = '';
+  displayDigits = ['', '', '', '', '', ''];
   currentPosition = 0;
 
   constructor(private router: Router, private location: Location) {}
-
-  get displayDigits(): string[] {
-    const digits = this.code.split('');
-    return Array(4).fill('').map((_, i) => digits[i] || '');
-  }
-
-  get isCodeValid(): boolean {
-    return this.code.length === 4;
-  }
-
-  isCurrentPosition(index: number): boolean {
-    return index === this.code.length;
-  }
 
   goBack() {
     this.location.back();
   }
 
-  onCodeInput(event: Event) {
-    const input = event.target as HTMLInputElement;
-    let value = input.value.replace(/\D/g, '');
-    value = value.slice(0, 4);
-    
-    this.code = value;
-    this.currentPosition = value.length;
-    input.value = this.code;
+  onCodeInput($event: Event) {
+    const input = $event.target as HTMLInputElement;
+    this.code = input.value.replace(/\D/g, '').slice(0, 6);
+    this.formattedCode = this.code;
+    this.displayDigits = [...this.code.padEnd(6, '').split('')];
+    this.currentPosition = this.code.length;
+  }
+
+  isCurrentPosition(i: number): boolean {
+    return i === this.currentPosition;
   }
 
   navigateNext() {
-    if (this.isCodeValid) {
+    if (this.code.length === 6) {
+      localStorage.setItem('verificationCode', this.code);
       this.router.navigate(['/password']);
     }
   }
