abc012daffe8bbcdf896b4172ceb6ac259d305d1
Adam El Karki <adamkar94@gmail.com>
2025-01-17 19:02:40 +0100

    create customer profile without nav

---
+++ src/app/customer-profile/customer-profile.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-profile',
+  templateUrl: './customer-profile.component.html',
+  styleUrls: ['./customer-profile.component.scss'],
+})
+export class CustomerProfileComponent {
+  constructor() {}
+
+  ngOnInit() {}
+}
