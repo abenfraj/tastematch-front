d5e5e3eba3b211140b409dda0374e4c6f61528ae
abenfraj <ayoub.benfraj013@gmail.com>
2025-01-18 15:17:15 +0100

    swipe improvements

--- src/app/swipe/swipe.component.ts
+++ src/app/swipe/swipe.component.ts
@@ -15,8 +15,29 @@ export class SwipeComponent {
     rating: 4.7,
     cuisine: 'French',
     distance: '0.1',
-    image: '/assets/samples/fouquets.png'
+    images: [
+      '/assets/samples/fouquets.png',
+      '/assets/samples/fouquets2.png',
+      '/assets/samples/fouquets.png',
+      '/assets/samples/fouquets2.png',
+      '/assets/samples/fouquets.png',
+      '/assets/samples/fouquets2.png',
+      '/assets/samples/fouquets.png',
+      '/assets/samples/fouquets2.png',
+    ]
   };
 
-  constructor() {}
+  currentImageIndex = 0;
+
+  nextImage() {
+    if (this.currentImageIndex < this.restaurant.images.length - 1) {
+      this.currentImageIndex++;
+    }
+  }
+
+  previousImage() {
+    if (this.currentImageIndex > 0) {
+      this.currentImageIndex--;
+    }
+  }
 }
